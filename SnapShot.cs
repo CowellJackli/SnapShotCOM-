55a596ac4405c1330356f179c528c9470622dbd3
CowellJackli <jackli@cowell.com.tw>
2015-07-07 20:53:10 +0800

    commit

--- SnapShot.cs
+++ SnapShot.cs
@@ -13,23 +13,12 @@ using System.IO;//引用COM+
 
 namespace SnapShotCom_
 {
-    /*      科威資訊股份有限公司 Cowell Infromation Ltd.
-     *      [版權所有 請勿盜用程式碼 本公司保有智慧財產權的相關追朔權利]
-     *      元件名稱 : SnapShotCOM
-     *      元件用途 : 提供截圖功能使用
-     *      作    者 : Jackli
-     *      日    期 : 2014/04/28
-     *      修改紀錄 :
-     *      ==============================================================================     
-     *      ==============================================================================      
-     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     public interface ISnapShot
     {
         string Url { get; set; }
         string FileName { get; set; }
         string Dir { get; set; }
         string ExePath { get; set; }
-
         string oErr { get; set; }
         void Save();
 
@@ -45,15 +34,13 @@ namespace SnapShotCom_
 
         public void Save()
         {
-
-
             try
             {
                 System.Diagnostics.Process p = new System.Diagnostics.Process();
-                p.StartInfo.WorkingDirectory = Application.StartupPath;    //要启动程序路径                
-                p.StartInfo.FileName = ExePath + @"\AutoRunSnapShot.exe";//需要启动的程序名   
-                p.StartInfo.Arguments = FileName + " " + Dir + " " + Url;//传递的参数       
-                p.Start();//启动      
+                p.StartInfo.WorkingDirectory = Application.StartupPath;      //啟動路徑             
+                p.StartInfo.FileName = ExePath + @"\AutoRunSnapShot.exe";   //啟動程序名稱
+                p.StartInfo.Arguments = FileName + " " + Dir + " " + Url;   //參數傳送       
+                p.Start();//開始運作      
             }
             catch (Exception ex)
             {
